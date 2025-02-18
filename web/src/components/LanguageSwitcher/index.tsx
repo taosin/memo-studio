a88c87bbf2d8c34ce88f9369101de6829b8ed8c0
xinxintao <xinxintao@foxmail.com>
2025-02-18 23:55:24 +0800

    style: update switch language css

--- web/src/components/LanguageSwitcher/index.tsx
+++ web/src/components/LanguageSwitcher/index.tsx
@@ -1,12 +1,13 @@
 import React, { useContext } from 'react';
 import { Button } from 'antd';
 import { LanguageContext } from '../../utils/LanguageContext';
+import styles from './LanguageSwitcher.module.scss';
 
 const LanguageSwitcher: React.FC = () => {
 	const { locale, setLocale } = useContext(LanguageContext);
 
 	return (
-		<div>
+		<div className={styles.switchLanguageContainer}>
 			<Button onClick={() => setLocale('en')} disabled={locale === 'en'}>
 				English
 			</Button>
