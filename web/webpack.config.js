b03d4fd46b74c132798037a80bcb8cf5552f1ba8
xinxintao <xinxintao@foxmail.com>
2025-04-16 08:47:18 +0800

    chore: update editor

--- web/webpack.config.js
+++ web/webpack.config.js
@@ -1,12 +1,28 @@
+const path = require("path");
+
 module.exports = {
   // 其他配置
   resolve: {
     fallback: {
-      crypto: require.resolve('crypto-browserify'),
-      stream: require.resolve('stream-browserify'),
+      crypto: require.resolve("crypto-browserify"),
+      stream: require.resolve("stream-browserify"),
     },
     external: {
-      'antd': 'antd',
-    }
+      antd: "antd",
+    },
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        include: [
+          path.resolve(__dirname, "src"),
+          path.resolve(__dirname, "node_modules/marked"),
+        ],
+        use: {
+          loader: "babel-loader",
+        },
+      },
+    ],
   },
 };
